1123f2123191fdcf7d525cea44a4d4ed98673d0b
Robbie Hott <jh2jf@virginia.edu>
2015-10-02 15:53:22 -0400

    Filled out skeleton Server and WebUI classes

--- src/snac/client/webui/WebUI.php
+++ src/snac/client/webui/WebUI.php
@@ -1,1 +1,36 @@
 <?php
+/*
+ * Web Interface Class File
+ *
+ * Contains the main web interface class that instantiates the web ui
+ *
+ * License:
+ * 
+ *
+ * @author Robbie Hott
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+ * @copyright 2015 the Rector and Visitors of the University of Virginia, and 
+ * the Regents of the University of California
+ */
+namespace \snac\server;
+
+
+/**
+ * WebUI Class
+ *
+ * This is the main web user interface class.  It should be instantiated, then the run() 
+ * method called to start the webui handler.
+ *
+ * @author Robbie Hott
+ */
+
+
+public class WebUI {
+
+
+    public function run() {
+
+        return;
+    }
+
+}
--- src/snac/server/Server.php
+++ src/snac/server/Server.php
@@ -1,1 +1,36 @@
 <?php
+/*
+ * Server Class File
+ *
+ * Contains the main server class that instantiates the main server
+ *
+ * License:
+ * 
+ *
+ * @author Robbie Hott
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+ * @copyright 2015 the Rector and Visitors of the University of Virginia, and 
+ * the Regents of the University of California
+ */
+namespace \snac\server;
+
+
+/**
+ * Server Class
+ *
+ * This is the main server class.  It should be instantiated, then the run() 
+ * method called to start the server running.
+ *
+ * @author Robbie Hott
+ */
+
+
+public class Server {
+
+
+    public function run() {
+
+        return;
+    }
+
+}
