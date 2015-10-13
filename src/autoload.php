fa9cd2f5c1143184422a4758d4c72c43bf167b53
Robbie Hott <jh2jf@virginia.edu>
2015-10-13 11:18:01 -0400

    Added eclipse run information for phpunit and autoload file for phpunit.

--- src/autoload.php
+++ src/autoload.php
@@ -1,6 +1,7 @@
 <?php
 // SNAC Autoload function
-function snac_autoload ($pClassName) {
-    include("" . str_replace("\\", "/", $pClassName) . ".php");
+function snac_autoload($pClassName) {
+
+    include ("" . str_replace("\\", "/", $pClassName) . ".php");
 }
 spl_autoload_register("snac_autoload");
