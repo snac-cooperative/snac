dba2f14ee350c6acc9f7b7c6dd399b9f9ba5540e
Robbie Hott <jh2jf@virginia.edu>
2015-11-04 11:38:33 -0500

    Constellation and Data Tests
    Added tests to test the flow from JSON to Array to Constellation and
    back.  These larger tests also tests other data structures
    included in the Constellation object, to ensure they also can correctly
    convert between JSON, Associative Array, and Data Structure.
    
    Also added larger test that runs 100 iterations over importing a large
    (12MB) JSON file (from Thomas Jefferson's EAC-CPF).

--- Unit Test Summary.md
+++ Unit Test Summary.md
@@ -1,13 +1,13 @@
 # Current Test Results
 
-Date: Tue Nov  3 17:05:48 EST 2015
+Date: Wed Nov  4 11:35:20 EST 2015
 
 ```
 PHPUnit 5.0.8 by Sebastian Bergmann and contributors.
 
-...........                                                       11 / 11 (100%)
+.................                                           17 / 17 (100%)
 
-Time: 26 ms, Memory: 4.00Mb
+Time: 17.03 seconds, Memory: 81.14Mb
 
-OK (11 tests, 13 assertions)
+OK (17 tests, 118 assertions)
 ```
--- test/snac/data/ConstellationTest.php
+++ test/snac/data/ConstellationTest.php
@@ -20,6 +20,32 @@
 class ConstellationTest extends PHPUnit_Framework_TestCase {
 
     /**
+     * Test that trying to read garbage instead of JSON results in not importing any data 
+     */
+    public function testJSONGarbage() {
+        $identity = new \snac\data\Constellation();
+        $jsonOrig = $identity->toJSON();
+
+
+        $identity->fromJSON("Garbage, not JSON");
+
+        $this->assertEquals($jsonOrig, $identity->toJSON());
+    }
+    
+    /**
+     * Test that trying to read empty JSON instead of Constellation JSON results in not importing any data 
+     */
+    public function testEmptyJSON() {
+        $identity = new \snac\data\Constellation();
+        $jsonOrig = $identity->toJSON();
+
+
+        $identity->fromJSON("{}");
+
+        $this->assertEquals($jsonOrig, $identity->toJSON());
+    }
+    
+    /**
      * Test that reading a JSON object, then serializing back to JSON gives the same result 
      */
     public function testJSONJSON() {
@@ -31,4 +57,67 @@ class ConstellationTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($jsonIn, $identity->toJSON());
     }
     
+    /**
+     * Test that reading a larger JSON object, then serializing back to JSON gives the same result 
+     */
+    public function testJSONJSON2() {
+        $identity = new \snac\data\Constellation();
+        $jsonIn = file_get_contents("test/snac/data/json/constellation_test2.json");
+        $arrayIn = json_decode($jsonIn, true);
+        $identity->fromJSON($jsonIn);
+
+        unset($jsonIn);
+        $arrayOut = json_decode($identity->toJSON(), true);
+
+        $this->assertEquals($arrayIn, $arrayOut);
+    }
+    
+    /**
+     * Test that reading a JSON object over another object will replace that object 
+     */
+    public function testJSONOverwrite() {
+        $identity = new \snac\data\Constellation();
+        $identity2 = new \snac\data\Constellation();
+        $jsonIn1 = file_get_contents("test/snac/data/json/constellation_test.json");
+        $jsonIn2 = file_get_contents("test/snac/data/json/constellation_test2.json");
+
+        $identity->fromJSON($jsonIn1);
+        $identity2->fromJSON($jsonIn2);
+        $identity2->fromJSON($jsonIn1);
+
+        $this->assertEquals($identity->toJSON(), $identity2->toJSON());
+    }
+    
+    /**
+     * Test that reading a larger JSON object multiple times does not result in memory error
+     */
+    public function testJSONExtreme() {
+
+        for ($i = 0; $i < 100; $i++) {
+            $identity = new \snac\data\Constellation();
+            $jsonIn = file_get_contents("test/snac/data/json/constellation_test2.json");
+            $identity->fromJSON($jsonIn);
+            $arrayIn = json_decode($jsonIn, true);
+            unset($jsonIn);
+            $this->assertEquals($arrayIn, $identity->toArray());
+            unset($identity);
+            unset($arrayIn);
+        }
+    }
+    
+    /**
+     * Test that reading a partial JSON Object works 
+     */
+    public function testPartialJSON() {
+        $identity = new \snac\data\Constellation();
+        $jsonIn = file_get_contents("test/snac/data/json/constellation_simple.json");
+
+        $identity->fromJSON($jsonIn);
+
+        $arrayIn = json_decode($jsonIn, true);
+        $idArray = $identity->toArray();
+
+        $this->assertEquals($arrayIn["nameEntries"], $idArray["nameEntries"]);
+    }
+
 }
