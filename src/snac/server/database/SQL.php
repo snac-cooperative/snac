fb0e4f638355af2d13ca843ff46f33117dd8c83d
Tom Laudeman <twl8n@virginia.edu>
2015-11-12 15:54:05 -0500

    fixing wrong var, also use a var that was ignored

--- src/snac/server/database/SQL.php
+++ src/snac/server/database/SQL.php
@@ -100,10 +100,13 @@ class SQL
                             'insert into otherid
                             (version, main_id, other_id, link_type)
                             values
-                            ($1, $2, $3, (select id from vocabulary where type=\'record_type\' and value=\'MergedRecord\'))');
+                            ($1, $2, $3, (select id from vocabulary where type=\'record_type\' and value=$4))');
         
         $result = $this->sdb->execute('query',
-                                      array($vh_info['id'], $vh_info['main_id'], $otherid));
+                                      array($vh_info['id'],
+                                            $vh_info['main_id'],
+                                            $href,
+                                            $type));
         $this->sdb->deallocate('query');
     }
     
