030d82e2ef62ddc8585fb532856ca2c6c2fbf39f
Tom Laudeman <twl8n@virginia.edu>
2015-11-18 11:11:45 -0500

    remove old debug messages

--- scripts/bulk_merge_ingest.php
+++ scripts/bulk_merge_ingest.php
@@ -121,7 +121,8 @@ function foo_main ()
         {
             $vh_info = $dbu->insertConstellation($id, $appUserID, $role, 'bulk ingest', 'bulk ingest of merged');
             check_vocabulary($id);
-            $msg = sprintf("File $file ok. vh_info: %s", var_export($vh_info, 1));
+            // $msg = sprintf("File $file ok. vh_info: %s", var_export($vh_info, 1));
+            $msg = sprintf("File $file ok.");
             quick_stderr($msg); // no terminal \n, the code will add that later
         }
         else
