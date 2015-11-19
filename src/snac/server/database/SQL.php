339cd9e69b8be965837bfdfd0a2ab44575fd37c0
Tom Laudeman <twl8n@virginia.edu>
2015-11-19 10:46:48 -0500

    remove debug

--- src/snac/server/database/SQL.php
+++ src/snac/server/database/SQL.php
@@ -358,8 +358,6 @@ class SQL
         $relationId = $row['id'];
         $this->sdb->deallocate($qq);
 
-        printf("typeof dates: %s\n", gettype($dates));
-
         foreach ($dates as $singleDate)
         {
             $date_fk = $this->insertDate($vh_info, $singleDate, 'related_identity', $relationId);
