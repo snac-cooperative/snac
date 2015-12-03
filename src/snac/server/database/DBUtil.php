d7f3529bbb34e7654f68eaf8374bd4279d6c99a9
Tom Laudeman <twl8n@virginia.edu>
2015-12-03 17:08:26 -0500

    move status messages out of this code

--- src/snac/server/database/DBUtil.php
+++ src/snac/server/database/DBUtil.php
@@ -183,7 +183,6 @@ class DBUtil
          */
 
         $cObj = new \snac\data\Constellation();
-        printf("Created an empty const: %s\n", json_encode($cObj, JSON_PRETTY_PRINT));
 
         $row = $this->sql->selectConstellation($vhInfo);
 
