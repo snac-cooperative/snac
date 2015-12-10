1dd26d0aa50810939b346dd8bf4582cbbd63f4c3
Robbie Hott <jh2jf@virginia.edu>
2015-12-10 09:42:36 -0500

    Updated doc/tests scripts to be local only, now that we have CI

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 vendor/bin/phpdoc -d src/ -t doc/ -i src/snac/Config.php
-rsync -av doc/ snac:/projects/snac_server/doc/
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-echo -e "# Current Test Results\n" > Unit\ Test\ Summary.md
-echo -e "Date: `date`\n" >> Unit\ Test\ Summary.md
-echo '```' >> Unit\ Test\ Summary.md
-vendor/bin/phpunit | tee -a Unit\ Test\ Summary.md
-echo '```' >> Unit\ Test\ Summary.md
+vendor/bin/phpunit 
