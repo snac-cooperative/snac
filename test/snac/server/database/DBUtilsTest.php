3dfc030938d2fea6213acfb23e9372c6ac5f85cd
Robbie Hott <jh2jf@virginia.edu>
2015-12-10 13:13:22 -0500

    Test Code Cleanup

--- test/snac/server/database/DBUtilsTest.php
+++ test/snac/server/database/DBUtilsTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Database Connector Test File
+ * Database Utility Test File
  *
  *
  * License:
@@ -10,9 +10,6 @@
  * @copyright 2015 the Rector and Visitors of the University of Virginia, and
  *            the Regents of the University of California
  */
-
-  // Is the use stmt part of setup?
-  // use \snac\server\database\DatabaseConnector;
 
 /**
  * Database Utils test suite
@@ -36,15 +33,12 @@ class DBUtilTest extends PHPUnit_Framework_TestCase {
      */
     public function testRandomConstellation() 
     {
-        printf("\nStarting DBUtil and SQL tests\n");
         $dbu = new snac\server\database\DBUtil();
         $this->assertNotNull($dbu);
-        // $this->fail("DBUtil object creation failed");
 
 
         list($appUserID, $role) = $dbu->getAppUserInfo('twl8n');
         $this->assertNotNull($appUserID);
-        // $this->fail("Failed to get appuser info for 'twl8n'");
 
 
         $vhInfo = $dbu->demoConstellation();
@@ -66,34 +60,5 @@ class DBUtilTest extends PHPUnit_Framework_TestCase {
         $selectedConstellationObj = $dbu->selectConstellation($vhInfo, $appUserID);
         $this->assertNotNull($selectedConstellationObj);
 
-        /* 
-         * examples from other code:
-         * $this->assertNotNull(json_decode($server->getResponse()));
-         * $this->assertEquals(
-         *     "pg_prepare(): Query failed: ERROR:  syntax error at or near \"NOT\"\nLINE 1: NOT A POSTGRES STATEMENT;\n        ^",
-         *     substr($message, 0));
-         * $this->fail("Failed to get a demo constellation");
-         */
     }
-
-    /*
-      public function testParseFile() {
-       $parser = new \snac\util\EACCPFParser();
-       try {
-           // Parse the file into an identity
-           $identity = $parser->parseFile("test/snac/util/eac-cpf/test1.xml");
-           
-           // Check that attributes matched the parsed versions
-           $this->assertAttributeEquals("http://n2t.net/ark:/99166/w6kw9c2x", "ark", $identity);
-           $this->assertAttributeEquals("person", "entityType", $identity);
-           $this->assertAttributeEquals("SNAC: Social Networks and Archival Context Project", "maintenanceAgency", $identity);
-           $this->assertAttributeEquals("English", "constellationLanguage", $identity);
-           $this->assertAttributeEquals("eng", "constellationLanguageCode", $identity);
-           $this->assertAttributeEquals("Latin Alphabet", "constellationScript", $identity);
-           $this->assertAttributeEquals("Latn", "constellationScriptCode", $identity);
-       } catch (\snac\exceptions\SNACParserException $e) {
-           $this->fail("Hit exception: " . $e->getMessage());
-       }
-    }
-    */
 }
