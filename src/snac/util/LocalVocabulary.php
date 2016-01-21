90a2569e1757af3d319544c956792efbe7fa67b0
Robbie Hott <jh2jf@virginia.edu>
2016-01-21 12:10:21 -0500

    Updated parser to correct controlled vocab types and set localvocab to organize vocabulary by type.

--- src/snac/util/LocalVocabulary.php
+++ src/snac/util/LocalVocabulary.php
@@ -7,19 +7,26 @@ class LocalVocabulary implements \snac\util\Vocabulary {
 
     public function __construct() {
         $db = new \snac\server\database\DBUtil();
-        $this->vocab = $db->getAllVocabulary();
+        $vocab = $db->getAllVocabulary();
+        // Fix up the vocabulary into a nested array
+        foreach($vocab as $v) {
+            if (!isset($this->vocab[$v["type"]]))
+                $this->vocab[$v["type"]] = array();
+            array_push($this->vocab[$v["type"]], array("id"=>$v["id"], "value"=>$v["value"]));
+        }
     }
 
 
     public function getTermByValue($value, $type) {
         $term = new \snac\data\Term();
-        foreach ($this->vocab as $k => $v) {
-            if ($v["term"] == $value) {
-                $term->setTerm($value);   
-                $term->setID($v["id"]);
-                return $term;
+        if (isset($this->vocab[$type]))
+            foreach ($this->vocab[$type] as $k => $v) {
+                if ($v["value"] == $value) {
+                    $term->setTerm($value);   
+                    $term->setID($v["id"]);
+                    return $term;
+                }
             }
-        }
         return $term;
     }
 
