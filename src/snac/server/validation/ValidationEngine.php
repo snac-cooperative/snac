221571a3b4a290106d90906c2433846617393129
Robbie Hott <jh2jf@virginia.edu>
2016-01-28 16:41:48 -0500

    Fleshing out validation engine and validator abstract class

--- src/snac/server/validation/ValidationEngine.php
+++ src/snac/server/validation/ValidationEngine.php
@@ -69,6 +69,104 @@ class ValidationEngine {
         if ($constellation == null || !($constellation instanceof \snac\data\Constellation))
             return false;
         
+        // For each validator, look over the constellation and validate it
+        foreach ($this->validators as $validator) {
+            
+            // Give the validator a chance to grab constellation-level data
+            $validator->setConstellation($constellation);
+            
+            // Validate constellation root
+            $validator->validateRoot($constellation);
+            
+            // Validate each piece of the constellation
+            foreach ($constellation->getBiogHistList() as $biogHist) {
+                $validator->validateBiogHist($biogHist);
+            }
+            
+            foreach ($constellation->getConventionDeclarations() as $cd) {
+                $validator->validateConventionDeclaration($cd);
+            }
+            
+            foreach ($constellation->getDateList() as $date) {
+                $validator->validateDate($date);
+            }
+            
+            foreach ($constellation->getFunctions() as $fn) {
+                $validator->validateFunction($fn);
+            }
+            
+            foreach ($constellation->getGenders() as $gender) {
+                $validator->validateGender($gender);
+            }
+            
+            foreach ($constellation->getGeneralContexts() as $gc) {
+                $validator->validateGeneralContext($gc);
+            }
+            
+            foreach ($constellation->getLanguagesUsed() as $languageUsed) {
+                $validator->validateLanguage($languageUsed);
+            }
+            
+            foreach ($constellation->getLegalStatuses() as $legalStatus) {
+                $validator->validateLegalStatus($legalStatus);
+            }
+            
+            foreach ($constellation->getMaintenanceEvents() as $maintenanceEvent) {
+                $validator->validateMaintenanceEvent($maintenanceEvent);
+            }
+            
+            foreach ($constellation->getMandates() as $mandate) {
+                $validator->validateMandate($mandate);
+            }
+            
+            foreach ($constellation->getNameEntries() as $nameEntry) {
+                $validator->validateNameEntry($nameEntry);
+            }
+            
+            foreach ($constellation->getNationalities() as $nationality) {
+                $validator->validateNationality($nationality);
+            }
+            
+            foreach ($constellation->getOccupations() as $occupation) {
+                $validator->validateOccupation($occupation);
+            }
+            
+            foreach ($constellation->getOtherRecordIDs() as $other) {
+                $validator->validateOtherRecordID($other);
+            }
+            
+            foreach ($constellation->getPlaces() as $place) {
+                $validator->validatePlace($place);
+            }
+            
+            foreach ($constellation->getRelations() as $relation) {
+                $validator->validateRelation($relation);
+            }
+            
+            foreach ($constellation->getResourceRelations() as $relation) {
+                $validator->validateResourceRelation($relation);
+            }
+            
+            foreach ($constellation->getSNACControlMetadata() as $scm) {
+                $validator->validateSNACControlMetadata($scm);
+            }
+            
+            foreach ($constellation->getSources() as $source) {
+                $validator->validateSource($source);
+            }
+            
+            foreach ($constellation->getStructureOrGenealogies() as $sog) {
+                $validator->validateStructureOrGenealogy($sog);
+            }
+            
+            foreach ($constellation->getSubjects() as $subject) {
+                $validator->validateSubject($subject);
+            }
+            
+        }
+        
+        
+        
         // Successful validation
         return true;
     }
--- src/snac/server/validation/validators/Validator.php
+++ src/snac/server/validation/validators/Validator.php
@@ -46,4 +46,117 @@ abstract class Validator {
         $this->validatorName = $name;
     }
     
+    /**
+     * Grab global constellation-level information that is needed in the validation
+     * 
+     * @param \snac\data\Constellation $constellation constellation
+     */
+    public function setConstellation($constellation);
+    
+    /**
+     * Validate the root of the constellation
+     * 
+     * @param \snac\data\Constellation $constellation constellation root to validate
+     */
+    public function validateRoot($constellation);
+    
+    /**
+     * Validate a biog hist
+     * 
+     * @param \snac\data\BiogHist $biogHist
+     */
+    public function validateBiogHist($biogHist);
+    
+    /**
+     * Validate a Convention Declaration
+     * 
+     * @param \snac\data\ConventionDeclaration $cd
+     */
+    public function validateConventionDeclaration($cd);
+    
+    /**
+     * Validate a Date
+     * 
+     * @param \snac\data\SNACDate $date
+     */
+    public function validateDate($date);
+    
+    /**
+     * Validate a Function
+     * @param \snac\data\SNACFunction $fn
+     */
+    public function validateFunction($fn);
+    
+    /**
+     * Validate a gender
+     * @param \snac\data\Gender $gender
+     */
+    public function validateGender($gender);
+    
+    /**
+     * Validate a general context
+     * @param \snac\data\GeneralContext $gc
+     */
+    public function validateGeneralContext($gc);
+    
+    /**
+     * Validate a language
+     * @param \snac\data\Language $lang
+     */
+    public function validateLanguage($lang);
+    
+    /**
+     * Validate a legal status
+     * @param \snac\data\LegalStatus $legalStatus
+     */
+    public function validateLegalStatus($legalStatus);
+    
+    /**
+     * Validate a Maintenance Event
+     * @param \snac\data\MaintenanceEvent $event
+     */
+    public function validateMaintenanceEvent($event);
+    
+    /**
+     * Validate a Mandate
+     * @param \snac\data\Mandate $mandate
+     */
+    public function validateMandate($mandate);
+    
+    /**
+     * Validate a Name Entry
+     * @param \snac\data\NameEntry $nameEntry
+     */
+    public function validateNameEntry($nameEntry);
+    
+    /**
+     * Validate a Nationality
+     * @param \snac\data\Nationality $nationality
+     */
+    public function validateNationality($nationality);
+    
+    /**
+     * Validate an Occupation
+     * @param \snac\data\Occupation $occupation
+     */
+    public function validateOccupation($occupation);
+    
+    public function validateOtherRecordID($other);
+    
+    public function validatePlace($place);
+    
+    public function validateRelation($relation);
+    
+    public function validateResourceRelation($relation);
+    
+    public function validateSNACControlMetadata($scm);
+    
+    public function validateSource($source);
+    
+    public function validateStructureOrGenealogy($sog);
+    
+    public function validateSubject($subject);
+    
+    public function validateTerm($term);
+    
 }
