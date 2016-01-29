892c57cb6f09278842d48808123416560ee508a8
Robbie Hott <jh2jf@virginia.edu>
2016-01-29 12:14:35 -0500

    Updated Validator to include optional validation context and more comments

--- src/snac/server/validation/validators/Validator.php
+++ src/snac/server/validation/validators/Validator.php
@@ -63,142 +63,165 @@ abstract class Validator {
     /**
      * Validate a biog hist
      * 
-     * @param \snac\data\BiogHist $biogHist
+     * @param \snac\data\BiogHist $biogHist BiogHist to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateBiogHist($biogHist);
+    public function validateBiogHist($biogHist, $context=null);
     
     /**
      * Validate a Convention Declaration
      * 
-     * @param \snac\data\ConventionDeclaration $cd
+     * @param \snac\data\ConventionDeclaration $cd ConventionDeclaration to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateConventionDeclaration($cd);
+    public function validateConventionDeclaration($cd, $context=null);
     
     /**
      * Validate a Date
      * 
-     * @param \snac\data\SNACDate $date
+     * @param \snac\data\SNACDate $date SNACDate to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateDate($date);
+    public function validateDate($date, $context=null);
     
     /**
      * Validate a Function
-     * @param \snac\data\SNACFunction $fn
+     * @param \snac\data\SNACFunction $fn SNACFunction to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateFunction($fn);
+    public function validateFunction($fn, $context=null);
     
     /**
      * Validate a gender
-     * @param \snac\data\Gender $gender
+     * @param \snac\data\Gender $gender Gender to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateGender($gender);
+    public function validateGender($gender, $context=null);
     
     /**
      * Validate a general context
-     * @param \snac\data\GeneralContext $gc
+     * @param \snac\data\GeneralContext $gc GeneralContext to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateGeneralContext($gc);
+    public function validateGeneralContext($gc, $context=null);
     
     /**
      * Validate a language
-     * @param \snac\data\Language $lang
+     * @param \snac\data\Language $lang Language to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateLanguage($lang);
+    public function validateLanguage($lang, $context=null);
     
     /**
      * Validate a legal status
-     * @param \snac\data\LegalStatus $legalStatus
+     * @param \snac\data\LegalStatus $legalStatus LegalStatus to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateLegalStatus($legalStatus);
+    public function validateLegalStatus($legalStatus, $context=null);
     
     /**
      * Validate a Maintenance Event
-     * @param \snac\data\MaintenanceEvent $event
+     * @param \snac\data\MaintenanceEvent $event MaintenanceEvent to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateMaintenanceEvent($event);
+    public function validateMaintenanceEvent($event, $context=null);
     
     /**
      * Validate a Mandate
-     * @param \snac\data\Mandate $mandate
+     * @param \snac\data\Mandate $mandate Mandate to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateMandate($mandate);
+    public function validateMandate($mandate, $context=null);
     
     /**
      * Validate a Name Entry
-     * @param \snac\data\NameEntry $nameEntry
+     * @param \snac\data\NameEntry $nameEntry NameEntry to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateNameEntry($nameEntry);
+    public function validateNameEntry($nameEntry, $context=null);
     
     /**
      * Validate a Nationality
-     * @param \snac\data\Nationality $nationality
+     * @param \snac\data\Nationality $nationality Nationality  to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateNationality($nationality);
+    public function validateNationality($nationality, $context=null);
     
     /**
      * Validate an Occupation
-     * @param \snac\data\Occupation $occupation
+     * @param \snac\data\Occupation $occupation Occupation to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateOccupation($occupation);
+    public function validateOccupation($occupation, $context=null);
     
     /**
      * validate an Other Record ID
-     * @param \snac\data\SameAs $other
+     * @param \snac\data\SameAs $other OtherID  to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateOtherRecordID($other);
+    public function validateOtherRecordID($other, $context=null);
     
     /**
      * Validate a Place
-     * @param \snac\data\Place $place
+     * @param \snac\data\Place $place Place to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validatePlace($place);
+    public function validatePlace($place, $context=null);
     
     /**
      * Validate a ConstellationRelation
-     * @param \snac\data\ConstellationRelation $relation
+     * @param \snac\data\ConstellationRelation $relation ConstellationRelation  to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateRelation($relation);
+    public function validateRelation($relation, $context=null);
     
     /**
      * Validate a Resource Relation
-     * @param \snac\data\ResourceRelation $relation
+     * @param \snac\data\ResourceRelation $relation ResourceRelation to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateResourceRelation($relation);
+    public function validateResourceRelation($relation, $context=null);
     
     /**
      * Validate a SCM Object
-     * @param \snac\data\SNACControlMetadata $scm
+     * @param \snac\data\SNACControlMetadata $scm Metadata to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateSNACControlMetadata($scm);
+    public function validateSNACControlMetadata($scm, $context=null);
     
     /**
      * Validate a Source
-     * @param \snac\data\Source $source
+     * @param \snac\data\Source $source Source to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateSource($source);
+    public function validateSource($source, $context=null);
     
     /**
      * Validate a StructureOrGenealogy
-     * @param \snac\data\StructureOrGenealogy $sog
+     * @param \snac\data\StructureOrGenealogy $sog StructureOrGenealogy to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateStructureOrGenealogy($sog);
+    public function validateStructureOrGenealogy($sog, $context=null);
     
     /**
      * Validate a Subject
-     * @param \snac\data\Subject $subject
+     * @param \snac\data\Subject $subject Subject to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateSubject($subject);
+    public function validateSubject($subject, $context=null);
     
     /**
      * Validate a Term 
-     * @param \snac\data\Term $term
+     * @param \snac\data\Term $term Term to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validateTerm($term);
+    public function validateTerm($term, $context=null);
     
     /**
      * Validate a PlaceEntry
-     * @param \snac\data\PlaceEntry $placeEntry
+     * @param \snac\data\PlaceEntry $placeEntry PlaceEntry to validate
+     * @param mixed[] $context optional Any context information needed for validation
      */
-    public function validatePlaceEntry($placeEntry);
+    public function validatePlaceEntry($placeEntry, $context=null);
     
 }
