ce3fd9d63e49bf765c0f90514fa94be23311b837
Robbie Hott <jh2jf@virginia.edu>
2016-03-31 11:23:44 -0400

    Updated install script to be automatable.  It also now clears the Elastic Search index.

--- install/install.php
+++ install/install.php
@@ -11,24 +11,24 @@
  * the Regents of the University of California
  */
 
+// Move to the directory containing this script
 chdir(dirname(__FILE__));
+
+// Autoload the SNAC server codebase
 include("../vendor/autoload.php");
 
 printf("basename: %s dirname: %s\n", basename(__FILE__), dirname(__FILE__));
 
-// include("src/snac/Config.php");
-
+use \snac\Config as Config;
 use \Monolog\Logger;
 use \Monolog\Handler\StreamHandler;
 
 // Set up the global log stream
 $log = new StreamHandler(\snac\Config::$LOG_DIR . \snac\Config::$SERVER_LOGFILE, Logger::DEBUG);
 
-// Path is relative to cwd when you launch the script, not the path of the script (this file).
-
-// The line below only works if you chdir() to the script's directory.
-
-use \snac\Config as Config;
+$automate = false;
+if ($argc == 2 && $argv[1] == "automate")
+    $automate = true;
 
 // Read the configuration file
 echo "Reading the configuration file in src/snac/Config.php.\n";
@@ -42,7 +42,12 @@ $user = Config::$DATABASE["user"];
 // Try to create the database
 
 echo "Would you like to try creating the PostgreSQL database?\n  ('yes' or 'no'): ";
-$response = trim(fgets(STDIN));
+$response = "no";
+if (!$automate)
+    $response = trim(fgets(STDIN));
+else
+    echo "no\n";
+
 if ($response == "yes") {
     echo "  Trying to create the database.  This script requires SUDO\n".
         "  privileges to switch to the postgres user to create the \n".
@@ -88,7 +93,12 @@ if ($dbHandle === false) {
 }
 
 echo "Would you like to load the schema (tables, indicies) into the database?\n  ('yes' or 'no'): ";
-$response = trim(fgets(STDIN));
+$response = "yes";
+if (!$automate)
+    $response = trim(fgets(STDIN));
+else
+    echo "yes\n";
+
 if ($response == "yes") {
     echo "  Running the SQL initialization script\n";
     $res = pg_query($dbHandle, file_get_contents("sql_files/schema.sql"));
@@ -105,7 +115,12 @@ if ($response == "yes") {
 } 
 
 echo "Would you like to load the vocabulary schema (and drop the existing tables)?\n  ('yes' or 'no'): ";
-$response = trim(fgets(STDIN));
+$response = "yes";
+if (!$automate)
+    $response = trim(fgets(STDIN));
+else
+    echo "yes\n";
+
 if ($response == "yes") {
     echo "  Dropping vocabulary, if exists, and creating new tables\n";
     $res = pg_query($dbHandle, file_get_contents("sql_files/vocabulary_init.sql"));
@@ -122,7 +137,12 @@ if ($response == "yes") {
 } 
 
 echo "Would you like to load the place vocabulary schema (and drop the existing tables)?\n  ('yes' or 'no'): ";
-$response = trim(fgets(STDIN));
+$response = "yes";
+if (!$automate)
+    $response = trim(fgets(STDIN));
+else
+    echo "yes\n";
+
 if ($response == "yes") {
     echo "  Dropping place vocabulary, if exists, and creating new tables\n";
     $res = pg_query($dbHandle, file_get_contents("sql_files/places_vocabulary_init.sql"));
@@ -141,7 +161,12 @@ if ($response == "yes") {
 
 
 echo "Would you like to load the initial users into the database?\n  ('yes' or 'no'): ";
-$response = trim(fgets(STDIN));
+$response = "yes";
+if (!$automate)
+    $response = trim(fgets(STDIN));
+else
+    echo "yes\n";
+
 if ($response == "yes") {
     echo "  Adding system-level role to the database\n";
     $res = pg_query($dbHandle, file_get_contents("sql_files/initialize_users.sql"));
@@ -159,7 +184,12 @@ if ($response == "yes") {
 }
 
 echo "Would you like to load the language codes into the database?\n  ('yes' or 'no'): ";
-$response = trim(fgets(STDIN));
+$response = "yes";
+if (!$automate)
+    $response = trim(fgets(STDIN));
+else
+    echo "yes\n";
+
 if ($response == "yes") {
     echo "  Adding language codes to the database\n";
     $res = pg_query($dbHandle, file_get_contents("sql_files/languages.sql"));
@@ -177,7 +207,12 @@ if ($response == "yes") {
 }
 
 echo "Would you like to load the script codes into the database?\n  ('yes' or 'no'): ";
-$response = trim(fgets(STDIN));
+$response = "yes";
+if (!$automate)
+    $response = trim(fgets(STDIN));
+else
+    echo "yes\n";
+
 if ($response == "yes") {
     echo "  Adding script codes to the database\n";
     $res = pg_query($dbHandle, file_get_contents("sql_files/scripts.sql"));
@@ -197,7 +232,12 @@ if ($response == "yes") {
 
 
 echo "Would you like to load the controlled vocabulary into the database?\n  ('yes' or 'no'): ";
-$response = trim(fgets(STDIN));
+$response = "yes";
+if (!$automate)
+    $response = trim(fgets(STDIN));
+else
+    echo "yes\n";
+
 if ($response == "yes") {
     echo "  Adding controlled vocabulary to the database\n";
     $res = pg_query($dbHandle, file_get_contents("sql_files/vocabulary.sql"));
@@ -216,7 +256,12 @@ if ($response == "yes") {
 
 
 echo "Would you like to load the controlled place vocabulary into the database?\n  ('yes' or 'no'): ";
-$response = trim(fgets(STDIN));
+$response = "yes";
+if (!$automate)
+    $response = trim(fgets(STDIN));
+else
+    echo "yes\n";
+
 if ($response == "yes") {
     echo "  Adding controlled place vocabulary to the database\n";
     $res = pg_query($dbHandle, file_get_contents("sql_files/places_vocabulary.sql"));
@@ -233,14 +278,48 @@ if ($response == "yes") {
     echo "  Not loading the controlled place vocabulary. They can be found in sql_files/places_vocabulary.sql.\n\n";
 }
 
+echo "Would you like to empty the Elastic Search Indices?\n  ('yes' or 'no'): ";
+$response = "yes";
+if (!$automate)
+    $response = trim(fgets(STDIN));
+else
+    echo "yes\n";
+
+if ($response == "yes" && \snac\Config::$USE_ELASTIC_SEARCH) {
+    echo "  Emptying the Elastic Search Indices\n";
+
+    $eSearch = \Elasticsearch\ClientBuilder::create()
+            ->setHosts([\snac\Config::$ELASTIC_SEARCH_URI])
+            ->setRetries(0)
+            ->build();
+
+    if ($eSearch != null) {
+        echo "   - connected to Elastic Search\n";
+        $params = array("index" => \snac\Config::$ELASTIC_SEARCH_BASE_INDEX);
+        $response = $eSearch->indices()->delete($params);
+        echo "   - deleted search index\n";
+        echo "  Successfully emptied the Elastic Search Indices.\n\n";
+    } else {
+        echo "  ERR: Unable to connect or delete Elastic Search index.\n";
+    }
+} else {
+    echo "  Not emptying the Elastic Search Indices.\n\n";
+}
 
 
 echo "Would you like to load a sampling of records into the database?\n ('yes' or 'no'): "; 
-$response = trim(fgets(STDIN));
+$response = "yes";
+if (!$automate)
+    $response = trim(fgets(STDIN));
+else
+    echo "yes\n";
+
 if ($response == "yes") {
     echo "  What is the full path to the SNAC merged CPF? [default: /data/merge/]\n  :";
-    $dir = trim(fgets(STDIN));
-    if ($dir == null || $dir = "")
+    $dir = null;
+    if (!$automate)
+        $dir = trim(fgets(STDIN));
+    if ($dir == null || $dir == "")
         $dir = "/data/merge/";
     $retval = 0;
     echo "  Attempting to ingest sample records from $dir.\n";
