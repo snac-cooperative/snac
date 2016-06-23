d9f60a47fc357e739170b2da24e0ead616cb8682
Tom Laudeman <twl8n@virginia.edu>
2016-06-23 16:41:36 -0400

    Move role and privilege inserts to a separate file. Update install.php to load them

--- install/sql_files/schema.sql
+++ install/sql_files/schema.sql
@@ -195,56 +195,6 @@ create table privilege_role_link (
         rid        int                -- fk to role.id
         );
 
-insert into privilege (label, description) values ('Edit', 'Edit constellations');
-insert into privilege (label, description) values ('Create', 'Create new constellations');
-insert into privilege (label, description) values ('Publish', 'Publish or commit a constellation after reviewing');
-insert into privilege (label, description) values ('Send for Review', 'Send a constellation to reviewer(s)');
-insert into privilege (label, description) values ('Simplified Create', 'Create basic, simplified, constellations');
-insert into privilege (label, description) values ('Suggest Edits', 'Suggest constellation edits');
-insert into privilege (label, description) values ('Change Locks', 'Change which user has a constellation locked');
-insert into privilege (label, description) values ('Unlock Currently Editing', 'Unlock constellations stuck in status Currently Editing');
-insert into privilege (label, description) values ('Add Users', 'Enrole new SNAC participants (new users), edit new user info');
-insert into privilege (label, description) values ('Assign Roles', 'Assign, modify user roles');
-insert into privilege (label, description) values ('Modify Users', 'Modify user email, phone numbers, affiliation, etc.');
-insert into privilege (label, description) values ('Inactivate Users', 'Able to inactivate user accounts');
-insert into privilege (label,description) values ('Manage Groups','Add users to groups, remove users from groups, create and delete groups');
-insert into privilege (label, description) values ('Manage My Group', 'Administer the membership of groups I belong to');
-
-
-insert into role (label, description) values ('Contributor', 'Create simplified constellations, suggest edits');
-insert into role (label, description) values ('Editor, Training', 'Editor in training');
-insert into role (label, description) values ('Editor, Full', 'Full editor');
-insert into role (label, description) values ('Reviewer', 'Editor and moderator');
-insert into role (label, description) values ('Administrator', 'Manage users, roles, groups');
-insert into role (label, description) values ('System Administrator', 'SNAC developers, super users');
-
--- Build privilege role links, that is: add privileges to each role.
-
-insert into privilege_role_link (rid, pid)
-select (select id from role where label='Contributor'), id from privilege where 
-    label in ('Simplified Create', 'Suggest Edits');
-
-insert into privilege_role_link (rid, pid)
-select (select id from role where label='Editor, Training'), id from privilege where 
-    label in ('Create', 'Edit');
-
-insert into privilege_role_link (rid, pid)
-select (select id from role where label='Editor, Full'), id from privilege where 
-    label in ('Create', 'Edit', 'Publish');
-
-insert into privilege_role_link (rid, pid)
-select (select id from role where label='Reviewer'), id from privilege where 
-    label in ('Create', 'Edit', 'Publish', 'Change Locks', 'Unlock Currently Editing');
-
-insert into privilege_role_link (rid, pid)
-select (select id from role where label='Administrator'), id from privilege where 
-    label in ('Add Users', 'Assign Roles', 'Modify Users', 'Manage Groups', 'Inactivate Users');
-
-insert into privilege_role_link 
-    (pid, rid) 
-    select id as pid, (select id from role where label='System Administrator') as rid from privilege;
-
-
 -- There may be multiple active sessions per user, so we need a separate table for sessions.
 
 create table session (
