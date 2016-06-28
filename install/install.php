732c1e5716866c35f6ccf51532e5d6948a6b42ae
Tom Laudeman <twl8n@virginia.edu>
2016-06-28 13:55:28 -0400

    Remove debug related to tracking down non-blocking STDIN

--- install/install.php
+++ install/install.php
@@ -444,10 +444,6 @@ echo "Would you like to load a set of users into the database?\n See setup_files
 $response = "yes";
 if (!$automate) {
     $response = trim(fgets(STDIN));
-    while ($response != "yes" && $response != "no") {
-        echo "Would you like to load a set of users into the database?\n See setup_files/users_dist.csv for the file format\n ('yes' or 'no'): "; 
-        $response = trim(fgets(STDIN));
-    }
 }
 else
     echo "yes\n";
