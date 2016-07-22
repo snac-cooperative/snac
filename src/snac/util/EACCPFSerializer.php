b08936f403a6bf62a46de5ecdfe06d82ce167ac1
Tom Laudeman <twl8n@virginia.edu>
2016-07-22 14:47:50 -0400

    Most CFP serializer prototype functionality working, checkpoint

---
+++ src/snac/util/EACCPFSerializer.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Serialize a PHP Constellation object to EAC-CPF XML
+ *
+ * Contains the serializer for into PHP Identity Constellation objects to EAC-CPF files.
+ *
+ * License:
+ *
+ *
+ * @author Tom Laudeman
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+ * @copyright 2016 the Rector and Visitors of the University of Virginia, and
+ *            the Regents of the University of California
+ */
+namespace snac\util;
+
+/**
+ * EAC-CPF Serializer
+ *
+ * Create EAC-CPF xml from a PHP Constellation object. Constellation objects are read out of the SQL database,
+ * so this completes the chain from SQL to XML.
+ *
+ * @author Tom Laudeman
+ *        
+ */
+class EACCPFSerializer {
+
+
+    public function __construct() {
+        
+    }
+
+
+}
