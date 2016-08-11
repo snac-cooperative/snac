b2a50d3872393cb328354168f3026d71c3fabdbf
Robbie Hott <jh2jf@virginia.edu>
2016-08-11 09:26:13 -0400

    Added delay in map drawing

--- src/virtualhosts/www/javascript/scripts.js
+++ src/virtualhosts/www/javascript/scripts.js
@@ -27,23 +27,27 @@ function openGeoPlaceViewer(id) {
                 geoMapView = null;
                 $("#geoPlaceMap").html("");
             }
-            // Create the Map and add it
-            geoMapView = L.map('geoPlaceMap').setView([data.term.latitude, data.term.longitude], 6);
-            L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            	maxZoom: 19,
-            	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(geoMapView);
-            var marker = L.marker([data.term.latitude, data.term.longitude]).addTo(geoMapView);
 
-            var html = "<h3>" + data.term.name + "</h3>" +
-                        "<p>" +
-                            "<strong>URI:</strong> " + data.term.uri + "<a class=\"label label-info\" target=\"_blank\" href=\"" + data.term.uri + "\">View</a>" + "<br/>" +
-                            "<strong>Administration Code:</strong> " + data.term.administrationCode + "<br/>" +
-                            "<strong>Country Code:</strong> " + data.term.countryCode + "<br/>" +
-                            "<strong>Location:</strong> (" + data.term.latitude + ", " + data.term.longitude + ")" +
-                        "</p>";
-            $("#geoPlaceInfo").html(html);
-            //$("#geoPlaceInfoPane").modal();
+            // Add a slight delay to the map viewing so that the modal window has time to load
+            setTimeout(function() {
+                // Create the Map and add it
+                geoMapView = L.map('geoPlaceMap').setView([data.term.latitude, data.term.longitude], 6);
+                L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 19,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                }).addTo(geoMapView);
+                var marker = L.marker([data.term.latitude, data.term.longitude]).addTo(geoMapView);
+
+                // Add the data about the GeoTerm
+                var html = "<h3>" + data.term.name + "</h3>" +
+                            "<p>" +
+                                "<strong>URI:</strong> " + data.term.uri + " <a class=\"label label-info\" target=\"_blank\" href=\"" + data.term.uri + "\">View</a>" + "<br/>" +
+                                "<strong>Administration Code:</strong> " + data.term.administrationCode + "<br/>" +
+                                "<strong>Country Code:</strong> " + data.term.countryCode + "<br/>" +
+                                "<strong>Location:</strong> (" + data.term.latitude + ", " + data.term.longitude + ")" +
+                            "</p>";
+                $("#geoPlaceInfo").html(html);
+            }, 200);
         }
 
     });
