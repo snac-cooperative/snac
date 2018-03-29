6089f4e2a5d8b21552f252e78ca38c0d296f2d1e
Robbie Hott <jh2jf@virginia.edu>
2018-03-29 13:52:14 -0400

    Updated the contact us form to give sending feedback

--- src/virtualhosts/www/javascript/contact.js
+++ src/virtualhosts/www/javascript/contact.js
@@ -19,11 +19,17 @@ function sendContactForm() {
         "token" : $("#g-recaptcha-response").val()
     };
 
+    $("#send_comment").prop("disabled", true).addClass("disabled");
+    $("#send_comment").html("<i class=\"fa fa-spinner fa-pulse fa-fw\"></i> Sending...");
+    
     $.post(snacUrl+"/feedback", feedbackBody, function (data) {
         if (data.result == "success") {
             // show success alert
             $("#comment_status_message").addClass("alert-success").html("<p>Message sent successfully.</p>");
             $('#comment_status_message').slideDown();
+            
+            $("#send_comment").html("<i class=\"fa fa-paper-plane-o\" aria-hidden=\"true\"></i> Send");
+            $("#send_comment").prop("disabled", false).removeClass("disabled");
         } else {
             // show an error alert
             $("#comment_status_message").addClass("alert-warning").html("<p>Error: "+data.message+"</p>");
