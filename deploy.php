9d13912f308213b010c79513cb9b3484cc3f6706
Jeff Mueller <jeff.mueller@simplethread.com>
2024-08-27 15:08:59 -0400

    First draft of GitHub Actions deploy setup

---
+++ deploy.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+require 'contrib/rsync.php';
+
+set('application', 'SNAC');
+set('ssh_multiplexing', true);
+set('repository', 'git@github.com:snac-cooperative/snac.git');
+
+set('rsync_src', function () {
+    return __DIR__;
+});
+
+add('rsync', [
+    'exclude' => [
+        '.git',
+        '/.env',
+        '/vendor/',
+        '.github',
+        'deploy.php',
+    ],
+]);
+
+task('deploy:secrets', function () {
+    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+    upload('.env', get('deploy_path') . '/shared');
+});
+
+host('snaccooperative.org')
+  ->set('hostname', 'snaccooperative.org')
+  ->set('labels', ['env' => 'production', 'stage' => 'production'])
+  ->set('remote_user', 'snacworker')
+  ->set('deploy_path', '/lv2/snac');
+
+host('snac-dev.iath.virginia.edu')
+  ->set('hostname', 'snac-dev.iath.virginia.edu')
+  ->set('labels', ['env' => 'development', 'stage' => 'development'])
+  ->set('remote_user', 'snacworker')
+  ->set('deploy_path', '/lv2/snac');
+
+after('deploy:failed', 'deploy:unlock');
+
+desc('Deploy the application');
+
+task('deploy', [
+    'deploy:unlock',
+    'deploy:prepare',
+    'deploy:secrets',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:view:cache',
+    'artisan:config:cache',
+    'artisan:migrate',
+    'artisan:queue:restart',
+    'deploy:symlink',
+    'deploy:cleanup',
+]);
